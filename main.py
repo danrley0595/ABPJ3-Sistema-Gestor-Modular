ef3e7d578555a45feb3c9b2210837fefdd83176d
danrley0595 <danrleycramos@gmail.com>
2026-03-01 19:00:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+# criação de uma lista para armazenar os livros
+livros = []
 
+#Laço para apresentar o menu enquanto condição verdadeira
+while True:
+    print("\n=== GESTOR DE BIBLIOTECA ===\n")
+    print("1 - Cadastrar livro")
+    print("2 - Listar livros")
+    print("3 - Emprestar livro")
+    print("4 - Devolver livro")
+    print("5 - Buscar livro por título")
+    print("6 - Sair")
+
+    # .Strip para tirar espaços em branco do começo e do fim
+    opcao = input("Digitar opção do menu(1 a 6):").strip()
+    # Só entra no if se for de 1 a 6, senão retorna no while
+    if opcao in ("1", "2", "3", "4", "5", "6"):
+        
+        match opcao:
+            case "1":
+                print("\n--- Cadastrar livro ---\n")
+                titulo = input("Título: ").strip()
+                autor = input("Autor: ").strip()
+                ano = input("Ano: ").strip()
+                
+                # adicona livro a lista
+                livros.append({
+                        "titulo": titulo,
+                        "autor": autor,
+                        "ano": ano,
+                        "status": "Disponível",
+                        "emprestado_para": ""
+                    })
+                print("Livro Cadastrado com Sucesso!")
